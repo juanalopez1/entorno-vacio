49660de1f8f3826f6051efa50bc38cfd50443da6
juanalopez1 <juana.lopez@correo.ucu.edu.uy>
2025-07-28 12:43:14 -0300

    cors

--- backend/src/plugins/cors.ts
+++ backend/src/plugins/cors.ts
@@ -3,8 +3,8 @@ import cors, { FastifyCorsOptions } from '@fastify/cors';
 
 export default fp<FastifyCorsOptions>(async (fastify) => {
   await fastify.register(cors, {
-    origin: true, // permite todos los orígenes (equivale a '*', pero compatible con credenciales)
-    credentials: true, // si necesitás enviar cookies, tokens, etc.
+    origin: true, 
+    credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     exposedHeaders: ['Content-Length', 'X-Kuma-Revision'],
