dd804ae4f750623f0eb5607bb547a9cf3041467d
juanalopez1 <juana.lopez@correo.ucu.edu.uy>
2025-07-30 18:42:48 -0300

    tryinf

--- backend/src/routes/root.ts
+++ backend/src/routes/root.ts
@@ -1,9 +1,24 @@
-import { FastifyPluginAsync } from 'fastify'
+import { FastifyPluginAsync } from "fastify";
+import { WebSocket } from "@fastify/websocket";
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
-}
+  fastify.register(async function () {
+    fastify.route({
+      method: "GET",
+      url: "/",
+      handler: async (request, reply) => {
+        return { root: true };
+      },
+      wsHandler: (socket: WebSocket, request) => {
+        socket.send("bienvenido cliente al server de fastify");
+        fastify.websocketServer.clients.forEach((c:any) => {
+          c.send(
+            "cantidad de clientes: " + fastify.websocketServer.clients.size
+          );
+        });
+      },
+    });
+  });
+};
 
-export default root
+export default root;
